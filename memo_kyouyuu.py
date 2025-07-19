86ad75120e8f5ebdd3f0c014513c1ea9862d349d
とむ <ttiih@outlook.jp>
2025-07-19 23:53:04 +0900

    test_ver1

--- memo_kyouyuu.py
+++ memo_kyouyuu.py
@@ -2,41 +2,6 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
-from hokan import customer
-
-# --- 認証情報読み込み ---
-google_cloud_secret = st.secrets["google_cloud"]
-service_account_info = {
-    "type": google_cloud_secret["type"],
-    "project_id": google_cloud_secret["project_id"],
-    "private_key_id": google_cloud_secret["private_key_id"],
-    "private_key": google_cloud_secret["private_key"],
-    "client_email": google_cloud_secret["client_email"],
-    "client_id": google_cloud_secret["client_id"],
-    "auth_uri": google_cloud_secret["auth_uri"],
-    "token_uri": google_cloud_secret["token_uri"],
-    "auth_provider_x509_cert_url": google_cloud_secret["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": google_cloud_secret["client_x509_cert_url"],
-    "universe_domain": google_cloud_secret["universe_domain"]
-}
-
-# ✅ キャッシュ付きシート取得関数
-@st.cache_resource
-def get_sheet():
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(
-        service_account_info,
-        [
-            "https://spreadsheets.google.com/feeds",
-            "https://www.googleapis.com/auth/spreadsheets",
-            "https://www.googleapis.com/auth/drive.file",
-            "https://www.googleapis.com/auth/drive"
-        ]
-    )
-    gc = gspread.authorize(creds)
-    return gc.open("memo_kyouyuu").sheet1
-
-sheet = get_sheet()
-
 ############################################
 day1 = "日付を選択してください"
 name = "入力してください"
