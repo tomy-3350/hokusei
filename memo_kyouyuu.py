8bc14337ba4f8dab35e3883617cbd30ad195366d
とむ <ttiih@outlook.jp>
2025-07-19 23:51:32 +0900

    test_ver1

--- memo_kyouyuu.py
+++ memo_kyouyuu.py
@@ -2,6 +2,8 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
+from hokan import customer
+
 # --- 認証情報読み込み ---
 google_cloud_secret = st.secrets["google_cloud"]
 service_account_info = {
@@ -34,3 +36,21 @@ def get_sheet():
     return gc.open("memo_kyouyuu").sheet1
 
 sheet = get_sheet()
+
+############################################
+day1 = "日付を選択してください"
+name = "入力してください"
+m_name = "入力してください"
+number = "入力してください"
+day2 = "日付を選択してください"
+memo = "入力してください"
+
+############################################
+st.title("金型メモ共有アプリ")
+st.text("型の改修内容や期限、出荷、トライ日程などメモ帳として使用してください")
+st.text("記入日、記入者名、メーカー、工番等、期限や日程、内容の順に入力してください")
+st.text("共有シートを開き、完了したものや取り消したいものはチェックをつけると取り消し線が入ります")
+
+day1 = st.date_input("日付を選択してください")
+
+name = st.text_input("入力してください")
