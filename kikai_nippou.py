79b09ceb3573ad0c9deb99959a354e7b600b3e12
とむ <ttiih@outlook.jp>
2025-07-19 21:40:37 +0900

    true_ver2.0

--- kikai_nippou.py
+++ kikai_nippou.py
@@ -66,7 +66,7 @@ def create_input_fields(index):
         key=f'genre_{index}'
     ) if customer != '選択してください' else '選択してください'
 
-    number = st.text_input(f'工番を入力{index}', key=f'number_{index}', placeholder="例: A1234") if genre != '選択してください' else ''
+    number = st.text_input(f'工番を入力{index}', key=f'number_{index}', placeholder="例: 51a111") if genre != '選択してください' else ''
 
     # --- 時間入力（プレースホルダ付きテキスト） ---
     time_input = st.text_input(f'時間を入力{index}', key=f'time_{index}', placeholder="例: 1.5")
